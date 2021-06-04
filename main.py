5c26184d76162204d91962aef0685b76f836d377
perunnial <jishnu.dave@gmail.com>
2021-06-04 17:57:23 +0530

    black formatter

--- main.py
+++ main.py
@@ -10,45 +10,55 @@ import urllib
 import json
 
 load_dotenv()
-bot = telepot.Bot(os.environ['BOT_TOKEN'])
+bot = telepot.Bot(os.environ["BOT_TOKEN"])
 nse = Nse()
+
 
 def get_nse_quote(ticker):
     try:
         quote = nse.get_quote(ticker)
         return quote
-    except(urllib.error.HTTPError):
-        print('HTTPError')
-    except(urllib.error.URLError):
-        print('URLError')
-    except(json.decoder.JSONDecodeError):
-        print('JSONDecodeError')
+    except (urllib.error.HTTPError):
+        print("HTTPError")
+    except (urllib.error.URLError):
+        print("URLError")
+    except (json.decoder.JSONDecodeError):
+        print("JSONDecodeError")
     except:
-        print('Exception')
+        print("Exception")
     return None
+
 
 def handle(msg):
     # pylint: disable=unbalanced-tuple-unpacking
     content_type, chat_type, chat_id = telepot.glance(msg)
-    response = 'unknown!'
-    if chat_type == 'private' and content_type == 'text':
-        msg_text = msg['text']
+    response = "unknown!"
+    if chat_type == "private" and content_type == "text":
+        msg_text = msg["text"]
         print(chat_id, msg_text)
-        if msg_text == '/start':
-            response = 'hello!'
+        if msg_text == "/start":
+            response = "hello!"
         elif not nse.is_valid_code(msg_text):
-            response = 'invalid!'
+            response = "invalid!"
         else:
             quote = get_nse_quote(msg_text)
             if quote is not None:
-                last_price = quote['lastPrice']
-                pct_change = str(quote['pChange'])
-                direction = '↑' if pct_change[0] != '-' else '↓'
-                response = '{:.2f}'.format(last_price) + ' ' + direction + ' ' + pct_change + '%'
+                last_price = quote["lastPrice"]
+                pct_change = str(quote["pChange"])
+                direction = "↑" if pct_change[0] != "-" else "↓"
+                response = (
+                    "{:.2f}".format(last_price)
+                    + " "
+                    + direction
+                    + " "
+                    + pct_change
+                    + "%"
+                )
             else:
-                response = 'unknown!'
+                response = "unknown!"
     print(response)
     bot.sendMessage(chat_id, response)
+
 
 MessageLoop(bot, handle).run_as_thread()
 while 1:
