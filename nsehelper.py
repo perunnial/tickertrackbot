fb23ccf7b4fc3921c8b272d12ea41e0415a82e5f
perunnial <jishnu.dave@gmail.com>
2021-06-09 21:49:03 +0530

    get index quote

--- nsehelper.py
+++ nsehelper.py
@@ -22,15 +22,34 @@ def get_quote(ticker):
     return None
 
 
+def get_index_quote(ticker):
+    try:
+        quote = nse.get_index_quote(ticker)
+        return quote
+    except (urllib.error.HTTPError):
+        print("HTTPError")
+    except (urllib.error.URLError):
+        print("URLError")
+    except (json.decoder.JSONDecodeError):
+        print("JSONDecodeError")
+    except:
+        print("Exception")
+    return None
+
+
 def is_valid_code(ticker):
     return nse.is_valid_code(ticker)
 
 
-def get_output(tickers):
+def get_output(tickers, is_index=False):
     output = ""
     for ticker in tickers:
         output += ticker + " "
-        quote = get_quote(ticker)
+        quote = {}
+        if is_index:
+            quote = get_index_quote(ticker)
+        else:
+            quote = get_quote(ticker)
         last_price = quote["lastPrice"]
         pct_change = str(quote["pChange"])
         direction = "↑" if pct_change[0] != "-" else "↓"
